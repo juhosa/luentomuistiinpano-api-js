78e2f8ac2580fa51d21ba099e1133c0385d76869
Juho S <juho.salli@gmail.com>
2023-11-23 11:52:01 +0200

    access-control-allow-origin header to functions

--- netlify/functions/courses/courses.js
+++ netlify/functions/courses/courses.js
@@ -1,20 +1,23 @@
 const handler = async (event) => {
   let courses = [
-    {id: 0, name: "versionhallinta"},
-    {id: 1, name: "java"},
-    {id: 2, name: "ruotsi"},
-    {id: 3, name: "ohjelmointi 1"},
-]
+    { id: 0, name: "versionhallinta" },
+    { id: 1, name: "java" },
+    { id: 2, name: "ruotsi" },
+    { id: 3, name: "ohjelmointi 1" },
+  ];
 
   try {
     return {
       statusCode: 200,
       body: JSON.stringify(courses),
-      headers: { "content-type": "application/json" },
-    }
+      headers: {
+        "access-control-allow-origin": "*",
+        "content-type": "application/json",
+      },
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
